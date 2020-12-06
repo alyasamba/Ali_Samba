c4f8b8ff46649aee67c4fb295510d8bd720bc677
alyasamba <sopesherifabdoulah@gmail.com>
2020-12-06 22:46:44 +0000

    ohhh la la

--- ali/ali/settings.py
+++ ali/ali/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['bayeali.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
